aad272b972d4ea5575d1a600462d83d3df9e749a
Ivan234dd <96702259+Ivan234dd@users.noreply.github.com>
2024-08-07 14:50:23 +0300

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,5 +1,5 @@
 TELEGRAM_ACCOUNTS = [
-    dict(phone='+99999999999', proxy="http://user:pass@host:port"),
+    dict(phone='+48735538445', proxy="http://mtddwniu:a1avaureymee@45.127.248.127:5128"),
     dict(phone='+99999999991', proxy="https://user:pass@host:port"),
     dict(phone='+55555555544'),
 ]
